51f99b08d6799d4518d94c8e7ad571212c392d2e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-07-04 18:36:01 +0900

    add library for loop

--- src/doubling.rs
+++ src/doubling.rs
@@ -11,3 +11,38 @@ fn doubling_template(mut k: usize, mut a: Vec<usize>) {
     }
     println!("{}", ans + 1);
 }
+
+// ダブリングじゃないけど、ループがある問題
+// init 始点の数(多くの問題の場合0かと)
+// n    要素数
+// k    k番目の数字が実質どれかを求める
+// map  次の要素がどれかを保持
+fn solve_loop(init: usize, n: usize, mut k: usize, map: Vec<usize>) -> usize {
+    let mut used = vec![false; n];
+    let mut path = vec![];
+    let mut next = init;
+    loop {
+        if used[next] {
+            break;
+        }
+        path.push(next);
+        used[next] = true;
+        next = map[next];
+    }
+    // ループのはじまり
+    let mut first = 0;
+    for i in 0..n {
+        if path[i] == next {
+            first = i;
+            break;
+        }
+    }
+    let loop_size = path.len() - first;
+    if k <= first {
+        return map[path[k]];
+    } else {
+        k -= first;
+        k %= loop_size;
+        return map[path[k + first]];
+    }
+}
