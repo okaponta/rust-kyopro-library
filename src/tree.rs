bca9a3bc5cccf7fc6d895b8e89f5807efdb28317
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-13 02:42:12 +0900

    add range seg

--- src/tree.rs
+++ src/tree.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
+use superslice::Ext;
+
 // segment tree
 // seg[0] -> seg[1]+seg[2]
 // seg[1] -> seg[3]+seg[4] seg[2] -> seg[5]+seg[6]
@@ -258,6 +261,51 @@ impl UnionFind {
     pub fn ds(&mut self, i: usize) -> &HashMap<usize, u32> {
         let par = self.find(i);
         &self.ds[par]
+    }
+}
+
+// 領域セグ木
+// 各ノードがソート済の配列をもっている
+// この実装だと指定の範囲のとある数字以下の数を数えることができる
+struct RangeSegTree {
+    data: Vec<Vec<i64>>,
+}
+
+impl RangeSegTree {
+    fn new(size: usize) -> RangeSegTree {
+        let m = size.next_power_of_two();
+        let data = vec![vec![]; m * 2];
+        RangeSegTree { data }
+    }
+
+    fn init(&mut self, a: &Vec<i64>, k: usize, l: usize, r: usize) {
+        if r - l == 1 {
+            self.data[k].push(a[l]);
+        } else {
+            let lch = k * 2 + 1;
+            let rch = k * 2 + 2;
+            let med = (l + r) / 2;
+            self.init(a, lch, l, med);
+            self.init(a, rch, med, r);
+            self.data[k] = self.data[lch]
+                .iter()
+                .merge(self.data[rch].iter())
+                .map(|i| *i)
+                .collect::<Vec<i64>>();
+        }
+    }
+
+    fn query(&self, i: usize, j: usize, x: i64, k: usize, l: usize, r: usize) -> usize {
+        if j <= l || r <= i {
+            0
+        } else if i <= l && r <= j {
+            self.data[k].upper_bound(&x)
+        } else {
+            let med = (l + r) / 2;
+            let lc = self.query(i, j, x, k * 2 + 1, l, med);
+            let rc = self.query(i, j, x, k * 2 + 2, med, r);
+            lc + rc
+        }
     }
 }
 
