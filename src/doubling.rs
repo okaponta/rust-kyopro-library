f71c54276bce1ff819d23074fdb98a6436b5478e
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-08-21 02:18:49 +0900

    add doubling

--- src/doubling.rs
+++ src/doubling.rs
@@ -12,6 +12,36 @@ fn doubling_template(mut k: usize, mut a: Vec<usize>) {
     println!("{}", ans + 1);
 }
 
+fn fibonacci(n: usize, modulo: usize) -> usize {
+    // フィボナッチの漸化式を行列で表現
+    let a = nalgebra::Matrix2::new(1, 1, 1, 0);
+    let a = pow(a, n, modulo);
+    *a.index((1, 0))
+}
+
+// 行列式のn乗
+fn pow(mut a: nalgebra::Matrix2<usize>, mut n: usize, modulo: usize) -> nalgebra::Matrix2<usize> {
+    let mut b = a.clone();
+    while 0 < n {
+        if n & 1 == 1 {
+            b = b * a;
+            rem(&mut b, modulo);
+        }
+        a = a * a;
+        rem(&mut a, modulo);
+        n >>= 1;
+    }
+    b
+}
+
+// 各項のmodをとる
+fn rem(a: &mut nalgebra::Matrix2<usize>, modulo: usize) {
+    a.m11 %= modulo;
+    a.m12 %= modulo;
+    a.m21 %= modulo;
+    a.m22 %= modulo;
+}
+
 // ダブリングじゃないけど、ループがある問題
 // init 始点の数(多くの問題の場合0かと)
 // n    要素数
