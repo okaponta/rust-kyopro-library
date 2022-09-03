76616c6daf4a3e42587b179d000de0c7024dbcdf
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-09-04 02:59:27 +0900

    add tree diameter

--- src/graph.rs
+++ src/graph.rs
@@ -330,6 +330,7 @@ fn prim_forest(n: usize, edges: Vec<Vec<(usize, i64)>>) -> i64 {
 pub struct LCA {
     n: usize,
     k: usize,
+    // とある頂点の1,2,4...個先の頂点を保持
     parent: Vec<Vec<usize>>,
     dist: Vec<usize>,
 }
@@ -346,9 +347,9 @@ impl LCA {
         Self { n, k, parent, dist }
     }
 
-    pub fn init(&mut self, edges: Vec<Vec<usize>>, init: usize) {
+    pub fn init(&mut self, edges: &Vec<Vec<usize>>, init: usize) {
         const INF: usize = 1 << 60;
-        self.dfs(init, init, &edges);
+        self.dfs(init, init, edges);
         for i in 0..self.k - 1 {
             for j in 0..self.n {
                 if self.parent[i][j] == INF {
@@ -362,9 +363,11 @@ impl LCA {
 
     // uとvのLCAを求める
     pub fn query(&self, mut u: usize, mut v: usize) -> usize {
+        // uを深い方として計算する
         if self.dist[u] < self.dist[v] {
             std::mem::swap(&mut u, &mut v);
         }
+        // LCAまでの距離を同じにする
         for i in 0..self.k {
             if (self.dist[u] - self.dist[v]) >> i & 1 == 1 {
                 u = self.parent[i][u];
@@ -373,6 +376,7 @@ impl LCA {
         if u == v {
             return u;
         }
+        // 一定以上の祖先は同じはずなので、上のbitから検証して揃えていく
         for i in (0..self.k).rev() {
             if self.parent[i][u] != self.parent[i][v] {
                 u = self.parent[i][u];
@@ -392,6 +396,7 @@ impl LCA {
         self.get_dist(u, p) + self.get_dist(p, v) == self.get_dist(u, v)
     }
 
+    // 根からの距離を求める
     fn dfs(&mut self, prev: usize, cur: usize, edges: &Vec<Vec<usize>>) {
         for &next in &edges[cur] {
             if next == prev {
@@ -402,6 +407,30 @@ impl LCA {
             self.dfs(cur, next, edges);
         }
     }
+}
+
+// 木の直径を求める。
+// 木の任意のノードからの距離の最大値はLかRへの距離となる
+// 参考：https://atcoder.jp/contests/abc267/submissions/34584812
+// 直径の片方ともう片方と直径
+fn tree_diameter(edges: &Vec<Vec<usize>>) -> (usize, usize, usize) {
+    let l = tree_diameter_dfs(edges, 0, !0);
+    let r = tree_diameter_dfs(edges, l.1, !0);
+    (l.1, r.1, r.0)
+}
+
+// (距離, to)の最大値を返却する
+fn tree_diameter_dfs(edges: &Vec<Vec<usize>>, cur: usize, parent: usize) -> (usize, usize) {
+    let mut ret = (0, cur);
+    for &to in &edges[cur] {
+        if to == parent {
+            continue;
+        }
+        let mut next = tree_diameter_dfs(edges, to, cur);
+        next.0 += 1;
+        ret = ret.max(next);
+    }
+    ret
 }
 
 #[cfg(test)]
@@ -557,7 +586,7 @@ mod tests {
             edges[v].push(u);
         }
         let mut lca = LCA::new(n);
-        lca.init(edges, 0);
+        lca.init(&edges, 0);
         assert_eq!(lca.query(1, 6), 0);
         assert_eq!(lca.query(4, 6), 2);
         assert_eq!(lca.query(4, 5), 3);
